9857a00d84205a966aa4418ea885b1e15735efcd
coDing-Knight99 <dhruvkanojiawork@gmail.com>
2025-09-01 22:26:57 +0530

    Deploy4

--- src/components/typingEffect.jsx
+++ src/components/typingEffect.jsx
@@ -2,10 +2,10 @@ import { ReactTyped } from "react-typed";
 
 const MyComponent = () => (
   <div>
-    <ReactTyped strings={["Hey there,"]} typeSpeed={40} showCursor={false} className="text-7xl text-[#00FFFF] " />
+    <ReactTyped strings={["Hey there,"]} typeSpeed={40} showCursor={false} className="text-6xl text-[#00FFFF] " />
     <br />
     <br />
-    <ReactTyped strings={["Myself"]} typeSpeed={40} showCursor={false} className="text-6xl text-white " />
+    <ReactTyped strings={["Myself"]} typeSpeed={40} showCursor={false} className="text-5xl text-white " />
 
     <ReactTyped
       strings={[
@@ -17,7 +17,7 @@ const MyComponent = () => (
       typeSpeed={40}
       backSpeed={50}
       loop
-      className="font-inter text-5xl text-red-600"
+      className="font-inter text-4xl text-red-600"
     >
       <input type="text" />
     </ReactTyped>
