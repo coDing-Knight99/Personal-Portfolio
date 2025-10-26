76e908d6086ff3a1845557e7835f54355b93ddb4
coDing-Knight99 <dhruvkanojiawork@gmail.com>
2025-10-26 10:04:18 +0530

    Added CodeCache

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { code, div } from 'framer-motion/client';
 import ProjectCard from './components/ProjectCard.jsx';
 import EducationCard from './components/EducationCard.jsx';
 import VantaHalo from './components/vantahalo.jsx';
-
 // const [menu, setmenu] = useState(false);
 
 function App() {
@@ -36,6 +35,7 @@ function App() {
 const projects=[
   {title:"CineRush", description:"A Movie Ticket Booking Platform",image:"https://dx35vtwkllhj9.cloudfront.net/universalstudios/jurassic-world-rebirth/images/regions/us/updates1/onesheet.jpg",demo:"https://cine-rush-six.vercel.app/",code:"https://github.com/coDing-Knight99/CineRush"},
   {title:"Twitter Clone", description:"A Twitter(X) Frontend Clone",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOfF9HqKARerlalRgX3icOlm2Dj3CXhcY7ai_picX_7KqawOrLcg2HelTKt8RjIfFwQVE&usqp=CAU",demo:"https://cine-rush-six.vercel.app/",code:"https://github.com/coDing-Knight99/CineRush"},
+  {title:"CodeCache", description:"A Code Snippet Organizer", image:"https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",demo:"https://code-cache-two.vercel.app/",code:"https://github.com/coDing-Knight99/CodeCache"},
   {title:"Project X",description:"Comming Soon...",image:"https://i.pinimg.com/564x/f9/28/e2/f928e27b6513d0d9c25a1b80293b12d1.jpg",demo:"",code:""},
 ]
   return (
@@ -125,7 +125,7 @@ Looking ahead, I aim to further deepen my skills in system design, software engi
 
       {/* Project Section */}
       <div id="Projects" className='scroll-mt-28 xl:mt-60 mt-30'>
-        <div className='mt-25 flex justify-center text-align-center'>
+        <div className='mt-25 flex'>
             <GradientText
         text="Projects"
         gradient="linear-gradient(90deg, #3b82f6 0%, #a855f7 50%, #ec4899 100%)"
